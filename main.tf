3575bd7a1118293058288fe83a100c931e5d0ab0
raanfefu <rafael.fernandez.ve@gmail.com>
2022-01-10 23:42:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,82 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features { 
+  }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  location            = var.location
+  resource_group_name = var.resourcegroup
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = var.resourcegroup
+  address_prefixes     = ["10.1.0.0/22"]
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.prefix}-k8s"
+  location            = var.location
+  resource_group_name = var.resourcegroup
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name           = "system"
+    node_count     = 1
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = azurerm_subnet.internal.id
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "calico"
+    load_balancer_sku = "standard"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  addon_profile {
+    aci_connector_linux {
+      enabled = false
+    }
+
+    azure_policy {
+      enabled = false
+    }
+
+    http_application_routing {
+      enabled = false
+    }
+
+    kube_dashboard {
+      enabled = false
+    }
+
+    oms_agent {
+      enabled = false
+    }
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "user" {
+  name                  = "user"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  vm_size               = "Standard_DS2_v2"
+  node_count            = 1
+  vnet_subnet_id        = azurerm_subnet.internal.id
+}
