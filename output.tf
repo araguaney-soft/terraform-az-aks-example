d1005d3cd2be136ac54bd5994a2cb8a999c5c683
raanfefu <rafael.fernandez.ve@gmail.com>
2022-01-10 23:42:27 -0500

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,27 @@
+output "id" {
+  value = azurerm_kubernetes_cluster.example.id
+}
+
+output "kube_config" {
+  sensitive = true
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+}
+
+output "client_key" {
+  sensitive = true
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
+}
+
+output "client_certificate" {
+  sensitive = true
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+  sensitive = true
+  value = azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.host
+}   
