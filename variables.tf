ea32f0cbd5ec8ef3b2bac36a105b2e8c06ec5232
raanfefu <rafael.fernandez.ve@gmail.com>
2022-01-29 22:35:43 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
 
+variable "prefix" {
+    type = string
+     default = "project"
+}
+
+variable "location" {
+  type = string
+  default = "eastus"
+}
+
+variable "resourcegroup" {
+  type = string
+  default = "1-09ef1fe5-playground-sandbox"
+ 
+}
